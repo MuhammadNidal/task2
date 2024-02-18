cfe18513493036ac8fb8a1b8dd2b0660ffb32d9c
unknown <mnidal416@gmail.com>
2024-02-18 14:29:38 +0500

    nidal

---
+++ script.js
@@ -0,0 +1,73 @@
+// function displayWelcomeMessage() {
+//     // Prompt the user to enter their name
+//     var userName = prompt("Enter your name:");
+  
+//     // Check if the user entered a name
+//     if (userName) {
+//       // Display a personalized welcome message in an alert
+//       alert("Welcome, " + userName + "!");
+//     } else {
+//       // Handle the case where the user didn't enter a name
+//       alert("Welcome!");
+//     }
+//   }
+  
+//   // Call the function to display the welcome message
+//   displayWelcomeMessage();
+  
+
+
+// Function to generate a random quote
+// function generateQuote() {
+//     // Array of predefined quotes
+//     var quotes = [
+//       "The only way to do great work is to love what you do. - Steve Jobs",
+//       "Believe you can and you're halfway there. - Theodore Roosevelt",
+//       "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+//       "In the middle of every difficulty lies opportunity. - Albert Einstein",
+//       "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+//       "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+//     ];
+  
+//     // Get a random index from the array
+//     var randomIndex = Math.floor(Math.random() * quotes.length);
+  
+//     // Return the randomly selected quote
+//     return quotes[randomIndex];
+//   }
+  
+//   // Display a random quote in the console
+//   function displayRandomQuote() {
+//     // Get the quote using the generateQuote function
+//     var randomQuote = generateQuote();
+  
+//     // Log the quote to the console
+//     console.log("Random Quote: " + randomQuote);
+//   }
+  
+//   // Call the function to display a random quote in the console
+//   displayRandomQuote();
+  
+
+
+// Function to generate a random number between 1 and 100
+// function generateRandomNumber() {
+//     // Generate a random number between 1 and 100
+//     var randomNumber = Math.floor(Math.random() * 100) + 1;
+  
+//     // Return the randomly generated number
+//     return randomNumber;
+//   }
+  
+//   // Display the randomly generated number in the console
+//   function displayRandomNumber() {
+//     // Get the random number using the generateRandomNumber function
+//     var randomNum = generateRandomNumber();
+  
+//     // Log the random number to the console
+//     console.log("Random Number: " + randomNum);
+//   }
+  
+//   // Call the function to display a random number in the console
+//   displayRandomNumber();
+  
